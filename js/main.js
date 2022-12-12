e475c943d464c71e6aa3f61b02a508d37c910360
Mohamed Mostafa <66911335+mohamedCleanCode@users.noreply.github.com>
2022-12-12 15:24:34 +0200

    Add some logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,99 @@
+// Main Varibles
 let form = document.querySelector("form");
-function sendData(e) {
-  let userName = document.querySelector("[name='user-name']").value;
-  let userEmail = document.querySelector("[name='user-email']").value;
-  let userPass = document.querySelector("[name='user-pass']").value;
-  let userPassConfirm = document.querySelector(
-    "[name='user-pass-confirm']"
-  ).value;
-  let lock = false;
-  let usernameRegX = /([a-z])+(\d*)?([a-z])/gi;
-  let result = usernameRegX.test(userName);
+let userName = document.querySelector("[name='user-name']");
+let userEmail = document.querySelector("[name='user-email']");
+let userPass = document.querySelector("[name='user-pass']");
+let userPassConfirm = document.querySelector("[name='user-pass-confirm']");
+let showBtns = document.querySelectorAll(".show");
+let link = document.querySelector(".link");
 
+console.log(true);
+// Function To Handel Data
+function sendData(e) {
+  let lock = false;
+  let active = document.querySelectorAll(".active");
+  if (active.length === 4) {
+    lock = true;
+    let data = {
+      username: userName.value.trim(),
+      email: userEmail.value,
+      password: userPass,
+      password_confirmation: userPassConfirm,
+    };
+    fetch("https://goldblv.com/api/hiring/tasks/register", {
+      method: POST,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    window.location.href = "../login.html";
+  }
   if (lock === false) {
     e.preventDefault();
-    console.log(`lock: ${lock}`);
-  } else {
+    alert("Fields Can't Be Empty");
   }
 }
+
+function handelUserNameFunc() {
+  let userNameRegX = /[a-z]+(\d+)?[a-z]/gi;
+  if (
+    userName.value.match(userNameRegX)[0] === userName.value &&
+    userName.value.length >= 5 &&
+    userName.value.length <= 15
+  ) {
+    HandelMarkRight(userName.parentElement.children[2]);
+  } else {
+    HandelMarkWrong(userName.parentElement.children[2]);
+  }
+}
+
+function handelUserEmailFunc() {
+  let userEmailRegX = /\w+@\w+.\w+/gi;
+  if (userEmail.value.match(userEmailRegX)[0] === userEmail.value) {
+    HandelMarkRight(userEmail.parentElement.children[2]);
+    localStorage.setItem("email", userEmail.value);
+  } else {
+    HandelMarkWrong(userEmail.parentElement.children[2]);
+  }
+}
+
+function handelUserPassFunc() {
+  if (userPass.value.length >= 8) {
+    HandelMarkRight(userPass.parentElement.children[2]);
+  } else {
+    HandelMarkWrong(userPass.parentElement.children[2]);
+  }
+}
+
+function handelUserPassConfirmFunc() {
+  if (userPassConfirm.value === userPass.value) {
+    HandelMarkRight(userPassConfirm.parentElement.children[2]);
+  } else {
+    HandelMarkWrong(userPassConfirm.parentElement.children[2]);
+  }
+}
+
 form.addEventListener("submit", sendData);
+userName.addEventListener("input", handelUserNameFunc);
+userEmail.addEventListener("input", handelUserEmailFunc);
+userPass.addEventListener("input", handelUserPassFunc);
+userPassConfirm.addEventListener("input", handelUserPassConfirmFunc);
+
+showBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.parentElement.parentElement.children[1].type === "password") {
+      e.target.parentElement.parentElement.children[1].type = "text";
+    } else {
+      e.target.parentElement.parentElement.children[1].type = "password";
+    }
+  });
+});
+
+function HandelMarkRight(ele) {
+  ele.className = "fa-solid fa-circle-check green active";
+}
+
+function HandelMarkWrong(ele) {
+  ele.className = "fa-solid fa-circle-xmark red";
+}
